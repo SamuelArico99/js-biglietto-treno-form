4210a44acc8524a00e799aa989b012bf784f051c
SamuelArico99 <samuel16699.sa@gmail.com>
2022-12-21 15:21:38 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const kmInput = document.getElementById('km-input')
+const ageInput = document.getElementById('age-input')
+
+const submitButton = document.getElementById('submit-button')
+
+submitButton.addEventListener('click',
+
+    function() {
+        console.log('kmInput.value', kmInput.value);
+        console.log('ageInput.value', ageInput.value);
+
+        const km = kmInput.value
+    }
+)
