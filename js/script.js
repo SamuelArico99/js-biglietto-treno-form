e3859c166bb8ce5add8d7aa4b72b8e9364af2ce6
SamuelArico99 <samuel16699.sa@gmail.com>
2022-12-21 16:23:04 +0100

    push

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const kmInput = document.getElementById('km-input')
 const ageInput = document.getElementById('age-input')
+const ageSelect = document.getElementById('age-select')
 
 const submitButton = document.getElementById('submit-button')
 
@@ -7,6 +8,29 @@ submitButton.addEventListener('click',
 
     function() {
         console.log('kmInput.value', kmInput.value);
-        console.log('ageInput.value', ageInput.value);
+        console.log('ageSelect.value', ageSelect.value);
+
+        const km = kmInput.value;
+        const age = ageSelect.value;
+
+        // Calcolo prezzo biglietto
+        const prezzo = 0.21;
+        let prezzofinale = prezzo * km;
+        console.log(prezzofinale.toFixed(2));
+        
+        
+        // Se il cliente è minorenne va applicato uno sconto del 20%
+        if(age =='m'){
+            prezzofinale = (prezzofinale -(prezzofinale / 100)*20);
+            console.log(prezzofinale.toFixed(2));
+        }
+        
+        // Se il cliente è over 65 va applicato uno sconto del 40%
+        else if(age == 'o'){
+            prezzofinale = (prezzofinale -(prezzofinale / 100)*40);
+            console.log(prezzofinale.toFixed(2));
+        }
+        // Output del prezzo con massimo due decimali
+        // Fine
     }
 );
